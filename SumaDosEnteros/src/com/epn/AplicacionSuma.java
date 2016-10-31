11a72caa43f3c22317e905712c50020317e79de4
belenquispi <belen.quispi@epn.edu.ec>
2016-10-31 10:38:27 -0500

    Corrección de Errores

--- SumaDosEnteros/src/com/epn/AplicacionSuma.java
+++ SumaDosEnteros/src/com/epn/AplicacionSuma.java
@@ -1,23 +1,19 @@
 package com.epn;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import javax.swing.JOptionPane;
 
 public class AplicacionSuma {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
-
-
-		int x, y;
+int x, y;
 		x= Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer entero: "));
 		y= Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo entero: "));
 
 
 		Entero numero1 = new Entero(x, y);
-		JOptionPane.showMessageDialog(null, "La suma de los dos enteros es: "+numero1.suma());
+		JOptionPane.showMessageDialog(null, "La suma de los dos enteros es: "+numero1.sumar());
 
 	}
-
 }
